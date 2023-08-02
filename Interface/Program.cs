1468f4949605b146fd44cce4d51f46e0df6c6e74
Ahmet <ahmetozaydn944@gmail.com>
2023-08-02 13:17:36 +0300

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿public interface IShape // interface is abstract 
+{
+    // It specifies what a class should do but not how it should do it.
+    // Interfaces allow you to achieve polymorphism
+    // cannot be created an object
+    // method overloading is also an example of polymorpholism
+    // part of Interface Segregation SOLID principle
+    void Draw(); // Method signature without implementation
+    double Area { get; } // Read-only property
+
+    // : Shape, Color multiple Inheritance
+}
---
+++ c-sharp_workspace/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args) // can get multiple string arguments
+    {
+        FunctionWithDefaultParametre(); // doesn't have to send a parametre for default parametres
+
+        int[] myArray = { 1, 2, 3, 4, 5 };
+        int[] anArray = new int[5];
+        int[] anArray2 = new int[5] { 2, 4, 7, 2, 3 }; // c style declaration
+
+
+        int number = 46;
+        Console.WriteLine(FunctionWithRefKeyword(ref number)); // ref or out keyword changes the value of a primitive type variable
+
+        foreach (int i in anArray2)
+        {
+            Console.Write($"{i} "); // foreach implementation
+        }
+
+
+        /*Template template = new Template
+        {
+            Name = "Ahmet",
+            Age = 24
+        }; */
+
+       // Console.WriteLine(template.Age);
+
+        /*
+         * function with default parametres, default parametres must placed to the end of parametre list
+         * if no parametre send the default values assigned to parametres.
+         */
+        static void FunctionWithDefaultParametre(string name = "Guest", int age = 23) // function with default params
+        {
+            Console.WriteLine($"Hello, {name}! You are {age} years old.");
+        }
+
+        static int FunctionWithRefKeyword(ref int number)
+        {
+            number = 40; // change the value of primitive type use it as reference variable
+            return number; // use primitive type as reference variable
+        }
+        /*
+         *  method overloading means change method signature and use it multiple times! Return type also can be changed
+         * In java return type cannot be changed to make method overloading
+         * Argument is actual values while parametres just placholders.
+         * In Java, method names is written with camelCase whereas in C# it is UpperCase
+         */
+
+    }
+       
+    }
+
+class Template
+{
+    // field, instance var in Java
+    private String _Name;
+    public String Name { // Encapsulation
+        set { _Name = value; } 
+        get { return "Mrs" + _Name; } 
+    } // getter and setter in C#
+    public int Age { set; get; } // default set and get methods, auto property
+                                  // field
+                                  //private String department;
+}
+
+
